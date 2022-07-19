424539da75c75449e6c45e2865347e185d5e1167
AhammedFayasTL <fayas1601@gmail.com>
2022-07-19 20:07:21 +0530

    3rd commit

---
+++ Training/src/practice/emailValidation.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class emailValidation {
+
+	public static void validate(String email, ArrayList<String> list) {
+		if (list.contains(email) == false)
+			System.out.println(email + " is not in the given list");
+		else
+			System.out.println(email + " is present in the given List");
+	}
+
+	public static void main(String[] args) {
+		ArrayList<String> emailList = new ArrayList<String>();
+		emailList.add("maruthy76@gmail.com");
+		emailList.add("honda01@gmail.com");
+		emailList.add("scania@gmail.com");
+
+		System.out.println("Enter an email id for validation : ");
+		Scanner sc = new Scanner(System.in);
+		String email = sc.nextLine();
+		validate(email, emailList);
+	}
+}
